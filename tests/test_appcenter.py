8f175f23e1e92822cdec4c53386a94b599b43c04
Dale Myers <dalemy@microsoft.com>
2019-10-04 15:28:59 +0100

    Bump dependencies

--- tests/test_appcenter.py
+++ tests/test_appcenter.py
@@ -23,7 +23,10 @@ def get_tokens() -> List[str]:
     :returns: The owner name, app name and token as a tuple.
     """
     try:
+        # pylint: disable=import-outside-toplevel
         import keyper
+
+        # pylint: enable=import-outside-toplevel
 
         return keyper.get_password(label="appcenter").split(":")
     except ImportError:
